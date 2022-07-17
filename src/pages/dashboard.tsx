03a678c0baa3b3a34613b601af524dbca803e327
Allan Sartor <allansartor@outlook.com>
2022-07-17 20:29:22 -0300

    Changes: Added theme context

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,65 +1,72 @@
-import { Box, SimpleGrid, useColorModeValue } from "@chakra-ui/react";
+import { useContext } from "react";
+
 import { GetServerSideProps } from "next";
-import { parseCookies } from 'nookies'
-import Card from "../components/Card";
+
+import { ThemeContext } from "../services/contexts/ThemeContext";
+
+import { parseCookies } from 'nookies';
+
+import { Box, SimpleGrid } from "@chakra-ui/react";
+
+import { Layout } from "../components/Layout";
 import { Charts } from "../components/Charts";
 import { Title } from "../components/Title";
 
 export default function Dashboard() {
-  const bg = useColorModeValue('gray.50', 'gray.800')
+  const { backgroundPrimary } = useContext(ThemeContext);
 
   return (
-    <Card>
+    <Layout>
       <SimpleGrid flex="1" gap="4" minChildWidth="360px" alignItems="flex-start">
-        <Title name="FinanceBarber | Dashboard"/>
+        <Title name="FinanceBarber | Dashboard" />
 
-          <Box
-            p={["6", "8"]}
-            bg={bg}
-            borderRadius={8}
-          >
-            <Charts title="Total de chamados hoje" />
-          </Box>
+        <Box
+          p={["6", "8"]}
+          bg={backgroundPrimary}
+          borderRadius={8}
+        >
+          <Charts title="Total de chamados hoje" />
+        </Box>
 
-          <Box
-            p={["6", "8"]}
-            bg={bg}
-            borderRadius={8}
-          >
-            <Charts title="Total de chamados na semana" />
-          </Box>
+        <Box
+          p={["6", "8"]}
+          bg={backgroundPrimary}
+          borderRadius={8}
+        >
+          <Charts title="Total de chamados na semana" />
+        </Box>
 
-          <Box
-            p={["6", "8"]}
-            bg={bg}
-            borderRadius={8}
-          >
-            <Charts title="Quantidade de chamados por setor" />
-          </Box>
+        <Box
+          p={["6", "8"]}
+          bg={backgroundPrimary}
+          borderRadius={8}
+        >
+          <Charts title="Quantidade de chamados por setor" />
+        </Box>
 
-          <Box
-            p={["6", "8"]}
-            bg={bg}
-            borderRadius={8}
-          >
-            <Charts title="Principais problemas" />
-          </Box>
+        <Box
+          p={["6", "8"]}
+          bg={backgroundPrimary}
+          borderRadius={8}
+        >
+          <Charts title="Principais problemas" />
+        </Box>
 
-          <Box
-            p={["6", "8"]}
-            bg={bg}
-            borderRadius={8}
-          >
-            <Charts title="Média de avaliações" />
-          </Box>
+        <Box
+          p={["6", "8"]}
+          bg={backgroundPrimary}
+          borderRadius={8}
+        >
+          <Charts title="Média de avaliações" />
+        </Box>
 
-        </SimpleGrid>
-    </Card>
+      </SimpleGrid>
+    </Layout>
   )
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const {['FinanceBarbertoken']: token } = parseCookies(context)
+  const { ['FinanceBarbertoken']: token } = parseCookies(context)
 
   if (!token) {
     return {
@@ -69,7 +76,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
       }
     }
   }
-  
+
   return {
     props: {}
   }
