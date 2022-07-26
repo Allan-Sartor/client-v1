750208f6625c46215fbbe171cd51474dc031b2d6
Allan-Sartor <allansartor@outlook.com>
2022-07-25 22:09:41 -0300

    Fix: Comment for page create user

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -19,7 +19,6 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { api } from "../../services/api";
-import { queryClient } from "../../services/queryClient";
 import { useRouter } from "next/router";
 import { GetServerSideProps } from "next";
 import { parseCookies } from "nookies";
@@ -41,108 +40,108 @@ const createUserFormSchema = yup.object().shape({
     .oneOf([null, yup.ref("password")], "As senhas precisam ser iguais"),
 });
 
-export default function CreateUser() {
-  const router = useRouter();
-  const bg = useColorModeValue('gray.50', 'gray.800');
+// export default function CreateUser() {
+//   const router = useRouter();
+//   const bg = useColorModeValue('gray.50', 'gray.800');
   
-  const createUser = useMutation(async (user: CreateUserFormData) => {
-    const response = await api.post('users', {
-      user: {
-        ...user,
-        create_at: new Date(),
-      }
-    })
+//   const createUser = useMutation(async (user: CreateUserFormData) => {
+//     const response = await api.post('users', {
+//       user: {
+//         ...user,
+//         create_at: new Date(),
+//       }
+//     })
 
-    return response.data.user;
-  }, {
-    onSuccess: () => {
-      queryClient.invalidateQueries('users')
-    }
-  });
+//     return response.data.user;
+//   }, {
+//     onSuccess: () => {
+//       queryClient.invalidateQueries('users')
+//     }
+//   });
 
-  const { register, handleSubmit, formState } = useForm({
-    resolver: yupResolver(createUserFormSchema),
-  });
+//   const { register, handleSubmit, formState } = useForm({
+//     resolver: yupResolver(createUserFormSchema),
+//   });
 
-  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
-    await createUser.mutateAsync(values);
+//   const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
+//     await createUser.mutateAsync(values);
 
-    router.push('/users');
-  }
+//     router.push('/users');
+//   }
 
-  const { errors } = formState;
+//   const { errors } = formState;
 
-  return (
-    <Box>
-      <Header />
+//   return (
+//     <Box>
+//       <Header />
 
-      <Flex w="100%" maxWidth={1480} my="6" mx="auto" px="6">
-        <Sidebar />
+//       <Flex w="100%" maxWidth={1480} my="6" mx="auto" px="6">
+//         <Sidebar />
 
-        <Box as="form" 
-          flex="1"
-          borderRadius={8}
-          bg={bg} 
-          p={["6", "8"]}
-          onSubmit={handleSubmit(handleCreateUser)}
-        >
-          <Heading size="lg" fontWeight="normal">
-            Criar usuário
-          </Heading>
+//         <Box as="form" 
+//           flex="1"
+//           borderRadius={8}
+//           bg={bg} 
+//           p={["6", "8"]}
+//           onSubmit={handleSubmit(handleCreateUser)}
+//         >
+//           <Heading size="lg" fontWeight="normal">
+//             Criar usuário
+//           </Heading>
 
-          <Divider my="6" borderColor="gray.700" />
+//           <Divider my="6" borderColor="gray.700" />
 
-          <VStack spacing="8">
-            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
-              <Input 
-                name="name"
-                type="text"
-                label="Nome completo"
-                error={errors.name} 
-                {...register("name")}
-              />
-              <Input
-                name="email"
-                type="email"
-                label="E-mail"
-                error={errors.email} 
-                {...register("email")}
-              />
-            </SimpleGrid>
+//           <VStack spacing="8">
+//             <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
+//               <Input 
+//                 name="name"
+//                 type="text"
+//                 label="Nome completo"
+//                 error={errors.name} 
+//                 {...register("name")}
+//               />
+//               <Input
+//                 name="email"
+//                 type="email"
+//                 label="E-mail"
+//                 error={errors.email} 
+//                 {...register("email")}
+//               />
+//             </SimpleGrid>
 
-            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
-              <Input 
-                name="password"
-                type="password"
-                label="Senha"
-                error={errors.password}
-                {...register("password")}
-              />
-              <Input
-                name="password_confirmation"
-                type="password"
-                label="Confirmar senha"
-                error={errors.password_confirmation}
-                {...register("password_confirmation")}
-              />
-            </SimpleGrid>
-          </VStack>
+//             <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
+//               <Input 
+//                 name="password"
+//                 type="password"
+//                 label="Senha"
+//                 error={errors.password}
+//                 {...register("password")}
+//               />
+//               <Input
+//                 name="password_confirmation"
+//                 type="password"
+//                 label="Confirmar senha"
+//                 error={errors.password_confirmation}
+//                 {...register("password_confirmation")}
+//               />
+//             </SimpleGrid>
+//           </VStack>
 
-          <Flex mt="8" justify="flex-end">
-            <HStack spacing="4">
-              <Link href="/users">
-                <Button as="a" colorScheme="whiteAlpha">
-                  Cancelar
-                </Button>
-              </Link>
-              <Button colorScheme="pink" type="submit" isLoading={formState.isSubmitting}>Salvar</Button>
-            </HStack>
-          </Flex>
-        </Box>
-      </Flex>
-    </Box>
-  );
-}
+//           <Flex mt="8" justify="flex-end">
+//             <HStack spacing="4">
+//               <Link href="/users">
+//                 <Button as="a" colorScheme="whiteAlpha">
+//                   Cancelar
+//                 </Button>
+//               </Link>
+//               <Button colorScheme="pink" type="submit" isLoading={formState.isSubmitting}>Salvar</Button>
+//             </HStack>
+//           </Flex>
+//         </Box>
+//       </Flex>
+//     </Box>
+//   );
+// }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const {['pontodesk.token']: token } = parseCookies(context)
