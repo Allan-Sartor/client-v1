4d79ac4f899e02b42af7ddd5d755a04fc0214237
Allan-Sartor <allansartor@outlook.com>
2022-07-27 14:42:05 -0300

    Fix: Adjust env routes for api

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -3,10 +3,21 @@ import { parseCookies } from "nookies";
 
 export function getAPIClient(context?: any) {
   const {'FinanceBarbertoken': token} = parseCookies(context);
+  const api = validOfEnvoriment();
+  
+  function validOfEnvoriment() {
+    let api;
 
-  const api = axios.create({
-    baseURL: 'http://localhost:3001/api/v1/'
-  })
+    if (process.env.NODE_ENV === 'production') {
+      return api = axios.create({
+        baseURL: 'https://api-lenhador.herokuapp.com/api/v1/'
+      })
+    } else if (process.env.NODE_ENV === 'development') {
+      return api = axios.create({
+        baseURL: 'http://localhost:3001/api/v1/'
+      })
+    }
+  }
   
   if (token) {
     api.defaults.headers['Authorization'] = `Bearer ${token}`;
